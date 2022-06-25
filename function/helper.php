b02b5c0e4189d55e8d2e2a8639050335aaac0ec9
ririeriska10 <eriskariri@gmail.com>
2022-06-25 21:42:11 +0700

    Projek Web2 dengan Prosedural

---
+++ function/helper.php
@@ -0,0 +1,5 @@
+<?php
+
+define("BASE_URL", "http://localhost/RIRI_WEB_NATIVE/");
+
+?>
---
+++ function/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "phpnativecrud";
+
+$koneksi = mysqli_connect($server, $username, $password, $dbname) or die("Koneksi ke database gagal"); 
+
+?>
